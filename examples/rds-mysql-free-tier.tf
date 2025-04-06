0b84d9d11aa26ffa747e595c3bb58c5b88ad3a41
karthiksakthiveltechie <karthiksakthiveltechie@gmail.com>
2025-04-06 09:10:26 +0530

    tested the general rds db with mysql code tested and updating to examples

---
+++ examples/rds-mysql-free-tier.tf
@@ -0,0 +1,16 @@
+module "rds_db_instance" {
+  source            = "terraform-aws-modules/rds/aws"
+  identifier        = "mylatestdefault"
+  allocated_storage = 20
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t4g.micro"
+  db_name           = "completeMysql"
+  username          = "complete_mysql"
+  password          = "Admin#1234!"
+
+  create_db_option_group    = false
+  create_db_parameter_group = false
+  manage_master_user_password = false
+  auto_minor_version_upgrade = false
+}
